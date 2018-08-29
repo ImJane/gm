60d0ab37fe07cee50dbd621aeeb3e85f1bc33a70
Edgar <15989069487@163.com>
2018-08-29 17:38:52 +0800

    删除node_modules

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var tools = require('./routes/tools');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+ 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/users', users); 
+app.use('/tools', tools); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,8 +45,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function(){
-  console.log('webapp listener port 3000!');
+app.listen(3000, function(){ 
+  console.log('webapp listener port 3000!!');
 })
 
 module.exports = app;
